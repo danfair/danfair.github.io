43033a509ec8065897b5108ad611a61c4a3bbdbc
Dan Fair <fair.dan@gmail.com>
2016-02-13 10:29:53 -0600

    added email form, style improvements

--- js/main.js
+++ js/main.js
@@ -26,6 +26,54 @@
                 scrollTop: $(e.target.hash).offset().top - 20
             }, 500);
 		});
+
+		$('.contact-form input, .contact-form textarea').blur(function() {
+			console.log('firing');
+			if ($(this).hasClass('invalid') && $(this).val() !== '') {
+				$(this).removeClass('invalid');
+			}
+		});
+
+		$('.js-contact-submit').on('click', function(e) {
+			e.preventDefault();
+			var form = $('.contact-form');
+			form.find('input').removeClass('invalid');
+
+			var formData = {};
+			formData.message = form.find('#message').val();
+			formData.email = form.find('#email').val();
+			formData.name = form.find('#name').val();
+
+			var validData = _validateForm(formData);
+			if (validData) {
+				$.ajax({
+				    url: "//formspree.io/fair.dan@gmail.com", 
+				    method: "POST",
+				    data: formData,
+				    dataType: "json",
+				    success: function(data) {
+				    	$('.contact-form').slideUp(500);
+				    	$('.contact-form__message.success').removeClass('hidden').fadeIn(500);
+				    }
+				});
+			} else {
+				return;
+			}
+		});
+	}
+
+	function _validateForm(formData) {
+		var returnVal = true;
+		for (var field in formData) {
+			if (!formData[field]) {
+				$('#' + field).addClass('invalid');
+				returnVal = false;
+			}
+		}
+		if (!returnVal) {
+			$('.js-validation-error').removeClass('hidden');
+		}
+		return returnVal;
 	}
 
 	$(document).ready(function() {   
